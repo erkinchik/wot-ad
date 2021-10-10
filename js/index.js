e8efab2c9f48a5aba6cb52d417f77ca49ff5b985
Erkin <erkin.berdibaev@gmail.com>
2021-10-10 22:59:20 +0600

    new fixes

--- js/index.js
+++ js/index.js
@@ -1,3 +1,29 @@
-// $(".wrapper").click(function () {
-//     $(this).hide("slow")
-// })
+// $(".decor_block-1").animate({
+//   background: red,
+// });
+
+gsap.from(".decor_block-1", 1.4, {
+  //   delay: 0.6,
+  //   opacity: 0,
+  x: -1200,
+  ease: Power4.linear,
+});
+gsap.from(".decor_block-2", 1.2, {
+    delay: 0.6,
+  //   opacity: 0,
+  x: -1200,
+  ease: Power4.linear,
+});
+gsap.from(".decor_block-3", 1.0, {
+    delay: 1.5,
+  //   opacity: 0,
+  x: -1800,
+  ease: Power4.linear,
+});
+gsap.from(".person_3", 0.4, {
+    delay: 2.2,
+  //   opacity: 0,
+  y: 380,
+  ease: Power4.linear,
+});
+
